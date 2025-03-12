73424372dba021d949105d899a5f7c76de6783bb
[debaditya-som] <[debaditya.som.22@aot.edu.in]>
2025-03-13 02:38:55 +0530

    added simple templates

---
+++ frontend/src/app/hospitals/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HospitalPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Hospital Page</h1>
+            <p>Welcome to the hospital page. Here you can find information about nearby hospitals and their services.</p>
+        </div>
+    );
+};
+
+export default HospitalPage;
---
+++ frontend/src/app/map/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MapPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Map Page</h1>
+            <p>This is the map page content.</p>
+        </div>
+    );
+};
+
+export default MapPage;
