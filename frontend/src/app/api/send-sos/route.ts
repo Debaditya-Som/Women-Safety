61ddfbf15d5d6a049fc427dd84cd00d2fc9caecb
Debaditya Som <debaditya.som@clientfabric.com>
2026-03-03 14:07:44 +0530

    twilio fix

--- frontend/src/app/api/send-sos/route.ts
+++ frontend/src/app/api/send-sos/route.ts
@@ -2,9 +2,9 @@ import { NextResponse } from "next/server";
 import twilio from "twilio";
 
 // Twilio Credentials
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const twilioPhoneNumber = process.env.NEXT_PUBLIC_TWILIO_PHONE_NUMBER;
+const accountSid = process.env.TWILIO_ACCOUNT_SID || "ACb9d44def9c80f8bab0f2b0e64c164104";
+const authToken = process.env.TWILIO_AUTH_TOKEN || "002031569669f19b3979812f25a575fc";
+const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER || "+14195975446";
 // Twilio Client
 const client = twilio(accountSid, authToken);
 
